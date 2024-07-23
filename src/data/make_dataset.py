f07a4fbf028863e2bac1a9a279e3d86de985cfb4
BOztasUK <berkanoztasuk@gmail.com>
2024-07-23 14:08:15 +0100

    Data visualisation

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -121,9 +121,9 @@ data_merged.columns = [
     "gyr_x",
     "gyr_y",
     "gyr_z",
+    "participant",
     "label",
     "category",
-    "participant",
     "set",
 ]
 
@@ -135,12 +135,13 @@ sampling = {
     "gyr_x": "mean",
     "gyr_y": "mean",
     "gyr_z": "mean",
+    "participant": "last",
     "label": "last",
     "category": "last",
-    "participant": "last",
     "set": "last",
 }
 
 data_reampled = data_merged.resample(rule="200ms").apply(sampling).dropna()
-
 data_reampled.info()
+
+data_reampled.to_pickle("../../data/interim/01_data_processed.pkl")
