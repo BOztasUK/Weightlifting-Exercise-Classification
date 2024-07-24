99915631cde49e87bf8160eccb3b15ecdc04ad5b
BOztasUK <berkanoztasuk@gmail.com>
2024-07-24 12:56:54 +0100

    complete feature engineering

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from DataTransformation import LowPassFilter, PrincipalComponentAnalysis
 from TemporalAbstraction import NumericalAbstraction
+from FequencyAbstraction import FourierTransformation
+from sklearn.cluster import KMeans
 
 df = pd.read_pickle("../../data/interim/02_removed_outliers_dataset.pkl")
 
@@ -11,7 +13,7 @@ predictor_columns = list(df.columns[:6])
 for col in predictor_columns:
     df[col] = df[col].interpolate()
 
-
+## add duration column
 for s in df["set"].unique():
 
     start = df[df["set"] == s].index[0]
@@ -39,7 +41,7 @@ for col in predictor_columns:
     del df_lowpass[col + "_lowpass"]
 
 
-# Apply PCA
+## Apply PCA
 df_pca = df_lowpass.copy()
 PCA = PrincipalComponentAnalysis()
 
@@ -55,10 +57,104 @@ plt.show()
 df_pca = PCA.apply_pca(df_pca, predictor_columns, 3)
 
 
-# creating sum of squares feature
+## creating sum of squares feature
 df_squared = df_pca.copy()
 acc_r = df_squared["acc_x"] ** 2 + df_squared["acc_y"] ** 2 + df_squared["acc_z"] ** 2
 gyr_r = df_squared["gyr_x"] ** 2 + df_squared["gyr_y"] ** 2 + df_squared["gyr_z"] ** 2
 
 df_squared["acc_r"] = np.sqrt(acc_r)
 df_squared["gyr_r"] = np.sqrt(gyr_r)
+
+## creating a rolling average (1 sec, 5 window size)
+df_temporal = df_squared.copy()
+NumAbs = NumericalAbstraction()
+
+predictor_columns = predictor_columns + ["acc_r", "gyr_r"]
+
+ws = 5
+
+df_temporal_list = []
+for s in df_temporal["set"].unique():
+    subset = df_temporal[df_temporal["set"] == s].copy()
+    for col in predictor_columns:
+        subset = NumAbs.abstract_numerical(subset, [col], ws, "mean")
+        subset = NumAbs.abstract_numerical(subset, [col], ws, "std")
+    df_temporal_list.append(subset)
+
+df_temporal = pd.concat(df_temporal_list)
+
+
+df_freq = df_temporal.copy().reset_index()
+FreqAbs = FourierTransformation()
+
+fs = int(1000 / 200)
+ws = int(2800 / 200)
+
+df_freq_list = []
+for s in df_freq["set"].unique():
+    print(f"Applying Fourier Transformation to set {s}")
+    subset = df_freq[df_freq["set"] == s].reset_index(drop=True)
+    subset = FreqAbs.abstract_frequency(subset, predictor_columns, ws, fs)
+    df_freq_list.append(subset)
+
+df_freq = pd.concat(df_freq_list).set_index("epoch (ms)", drop=True)
+
+
+## Handling overlapping windows
+df_freq = df_freq.dropna()
+
+# useing every 2nd row ie skipping one: using 50% of the data to reduce overfitting
+df_freq = df_freq.iloc[::2]
+
+
+## K-means Clustering
+df_cluster = df_freq.copy()
+
+cluster_columns = ["acc_x", "acc_y", "acc_z"]
+k_values = range(2, 10)
+inertias = []
+
+for k in k_values:
+    subset = df_cluster[cluster_columns]
+    kmeans = KMeans(n_clusters=k, n_init=20, random_state=0)
+    cluster_labels = kmeans.fit_predict(subset)
+    inertias.append(kmeans.inertia_)
+
+# plotting to identify optimal amount of clusters
+plt.figure(figsize=(10, 10))
+plt.plot(k_values, inertias)
+plt.ylabel("Sum of squared distances")
+plt.show()
+
+# chose 5 clusters usung elbow technique
+kmeans = KMeans(n_clusters=5, n_init=20, random_state=0)
+subset = df_cluster[cluster_columns]
+df_cluster["cluster"] = kmeans.fit_predict(subset)
+
+# Plot clusters of K-means
+fig = plt.figure(figsize=(15, 15))
+ax = fig.add_subplot(projection="3d")
+for c in df_cluster["cluster"].unique():
+    subset = df_cluster[df_cluster["cluster"] == c]
+    ax.scatter(subset["acc_x"], subset["acc_y"], subset["acc_z"], label=c)
+ax.set_xlabel("X-axis")
+ax.set_ylabel("Y-axis")
+ax.set_zlabel("Z-axis")
+plt.legend()
+plt.show()
+
+
+# Plot actual clusters using the label to compare to K-Means, subset by label
+fig = plt.figure(figsize=(15, 15))
+ax = fig.add_subplot(projection="3d")
+for l in df_cluster["label"].unique():
+    subset = df_cluster[df_cluster["label"] == l]
+    ax.scatter(subset["acc_x"], subset["acc_y"], subset["acc_z"], label=l)
+ax.set_xlabel("X-axis")
+ax.set_ylabel("Y-axis")
+ax.set_zlabel("Z-axis")
+plt.legend()
+plt.show()
+
+
+df_cluster.to_pickle("../../data/interim/03_feature_engineering_data.pkl")
